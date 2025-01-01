7ee850de3a06527aad37acdb57283412271b92c7
zidanali111 <zidanali111@gmail.com>
2025-01-01 19:09:55 +0530

    user-app added

---
+++ packages/db/prisma/migrations/20250101132533_mercant_and_user_added/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "email" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "auth_type" "AuthType" NOT NULL,
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
